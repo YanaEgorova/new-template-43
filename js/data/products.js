78d29597c374c9fd7a5f89f5acfc004801321fa0
Yana <yanaegorova69.3@gmail.com>
2021-10-13 01:20:57 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,160 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Face Cover',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Pack of 30.',
+            },
+            {
+                text: '100% Cotton.',
+            },
+            {
+                text: 'Imported.',
+            },
+            {
+                text: 'This mask is not intended for medical use, and not proven to reduce the transmission of disease.',
+            },
+            {
+                text: 'The CDC recommends use of cotton or cloth face coverings in public to reduce community spread by asymptomatic persons.',
+            },
+            {
+                text: 'One size fits most. Best for small-to-average size faces (7” wide x 5” tall).',
+            },
+            {
+                text: 'Reusable and machine washable: Handwash in warm and hang dry up to 15 times before discarding. Do not use bleach, chemical, or disinfectant to wash.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png', './img/img1-4.png', './img/img1-5.png', './img/img1-6.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Pliers',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                title: 'Heavy-Duty',
+                text: 'This tool works with various types of wire, allowing you to create beautiful jewelry.',
+            },
+            {
+                title: 'Eye Pin Tool',
+                text: 'These wire-shaping pliers make it easy to craft eye pins, bend metal, and trim wire.',
+            },
+            {
+                title: 'Craft & Jewel Accessory',
+                text: 'These pliers are great for jewelry kits for professional or hobby crafters.',
+            },
+            {
+                title: 'Consistent Loop Size',
+                text: 'Our looper pliers make it easy to make pin and eye holes of the same size.',
+            },
+            {
+                title: '3 mm Loop Tool',
+                text: 'This tool features a hole in the upper jaw, which enables use with long wire.',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png', './img/img2-3.png', './img/img2-4.png', './img/img2-5.png', './img/img2-6.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Storage Easel',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                title: 'Age Range',
+                text: 'Recommended for ages 3 to 6.',
+            },
+            {
+                title: 'Double The Fun',
+                text: 'This 2 sided easel features a chalkboard side and a magnetic, dry erase whiteboard side.',
+            },
+            {
+                title: 'What\'s Included',
+                text: 'Easel; bins and pull down paper roll with 33 feet of paper.',
+            },
+            {
+                title: 'Easily Store Supplies',
+                text: '2 convenient fabric storage bins keep crayons, markers, extra paper, paint or toys within reach.',
+            },
+            {
+                title: 'Quality Craftsmanship',
+                text: 'Made from strong wood, this easel is durable and built for years of use.',
+            },
+            {
+                title: 'Ideal Size',
+                text: 'Assembled Dimensions: 21.625 W x 20.5 D x 40.75 H Inches.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Model, Fish',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                text: 'The basic anatomy of fish including removable parts for much easier examination.',
+            },
+            {
+                text: 'The realistic, life-size model allows students to remove certain organs and examine both internal and external structures and anatomy of fish.',
+            },
+            {
+                text: 'Item Weight: 3.70 pounds.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Mobile Printer',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Prints photos directly from Bluetooth-enabled devices.',
+            },
+            {
+                text: 'Small, light and can be carried anywhere.',
+            },
+            {
+                text: '3. 4 in. x 2. 1 in. photo prints.',
+            },
+            {
+                text: 'All-in-one paper and ink cartridge.',
+            },
+            {
+                text: 'High-quality waterproof, smudgeproof and fingerprint-resistant photo prints.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-4.png', './img/img5-5.png', './img/img5-6.png', './img/img5-7.png', './img/img5-8.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Grooming Table',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'These adjustable-height tables are perfect for groomers of all sizes!',
+            },
+            {
+                text: 'Height adjusts for 24" to 34½"',
+            },
+            {
+                text: 'Tabletop is made of high-grade plywood covered in durable, non-slip, pebbled vinyl.',
+            },
+            {
+                text: 'Sturdy, powder-coated legs won\'t wobble.',
+            },
+            {
+                text: 'Folds quickly for convenient transport and easy grooming on the go.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png','./img/img6-3.png','./img/img6-4.png'],
+    },
+]
